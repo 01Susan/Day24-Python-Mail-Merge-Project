653aae0f35672c1292fe917b8b307a794efb8419
Susan <dhitalsusan2@gmail.com>
2023-01-16 10:49:00 +0545

    added some comment

--- Mail+Merge+Project+Start/Mail Merge Project Start/main.py
+++ Mail+Merge+Project+Start/Mail Merge Project Start/main.py
@@ -13,17 +13,20 @@ def reading_invited_names():
             names_invited.append(names)
 
 
+# TODO 2. Replacing [name] by user_name
 def replacing_name():
     with open(r".\Input\Letters\starting_letter.txt", mode='r') as reading_letter:
         reading_mail = reading_letter.read()
         return reading_mail
 
 
+# TODO 3. Making each file for each emails
 def ready_to_send():
     reading_invited_names()
     for name in names_invited:
         replace_space = name.replace(" ", "_")
         invited_name = replace_space.strip("\n")
+        # Path for creating each user files that consist email
         email_to = r".\Output\ReadyToSend\letter_to_" + invited_name + ".txt"
         with open(email_to, mode="w") as write_emails:
             mail_name = replacing_name().replace("[name]", invited_name)
