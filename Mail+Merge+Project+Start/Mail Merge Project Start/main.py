05605bd690fb369c8d667059bbfe2dfdf990f449
Susan <dhitalsusan2@gmail.com>
2023-01-16 10:43:56 +0545

    mail name automation

---
+++ Mail+Merge+Project+Start/Mail Merge Project Start/main.py
@@ -0,0 +1,33 @@
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+# TODO 1. Reading  all the name from invited_names.txt
+names_invited = []
+
+
+def reading_invited_names():
+    with open(r".\Input\Names\invited_names.txt", mode="r") as invited_names:
+        for names in invited_names:
+            names_invited.append(names)
+
+
+def replacing_name():
+    with open(r".\Input\Letters\starting_letter.txt", mode='r') as reading_letter:
+        reading_mail = reading_letter.read()
+        return reading_mail
+
+
+def ready_to_send():
+    reading_invited_names()
+    for name in names_invited:
+        replace_space = name.replace(" ", "_")
+        invited_name = replace_space.strip("\n")
+        email_to = r".\Output\ReadyToSend\letter_to_" + invited_name + ".txt"
+        with open(email_to, mode="w") as write_emails:
+            mail_name = replacing_name().replace("[name]", invited_name)
+            write_emails.write(mail_name)
+
+
+ready_to_send()
